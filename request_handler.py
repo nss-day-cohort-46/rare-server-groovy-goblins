8d4593b043f8e4b54aa3cca45453464e4e6068cc
daprieu <daprieu@gmail.com>
2021-04-26 11:38:43 -0500

    removed import statements at top of request handler becasue watchgod was angry

--- request_handler.py
+++ request_handler.py
@@ -1,13 +1,13 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import (get_all_animals, get_single_animal, create_animal, 
-                    delete_animal, update_animal, get_animals_by_location_id, get_animals_by_status)
-from locations import (get_all_locations, get_single_location, 
-                    create_location, delete_location, update_location)
-from employees import (get_all_employees, get_single_employee, 
-                    create_employee, delete_employee, update_employee, get_employees_by_location_id)
-from customers import (get_all_customers, get_single_customer, create_customer, 
-                    delete_customer, update_customer, get_customers_by_email)
+# from animals import (get_all_animals, get_single_animal, create_animal, 
+#                     delete_animal, update_animal, get_animals_by_location_id, get_animals_by_status)
+# from locations import (get_all_locations, get_single_location, 
+#                     create_location, delete_location, update_location)
+# from employees import (get_all_employees, get_single_employee, 
+#                     create_employee, delete_employee, update_employee, get_employees_by_location_id)
+# from customers import (get_all_customers, get_single_customer, create_customer, 
+#                     delete_customer, update_customer, get_customers_by_email)
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
