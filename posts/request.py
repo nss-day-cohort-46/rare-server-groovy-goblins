53235e07dd43e18221babe102b77a82cda289ce4
hrlnsnchz <hrlnsnchz@gmail.com>
2021-04-27 14:07:53 -0500

    Added an image_url column to sql string in the 'posts/request.py' file in the 'create_post()' function

--- posts/request.py
+++ posts/request.py
@@ -8,13 +8,14 @@ def create_post(new_post):
 
         db_cursor.execute("""
         INSERT INTO Posts
-            ( user_id, category_id, title, publication_date, content, approved )
+            ( user_id, category_id, title, publication_date, image_url, content, approved )
         VALUES
-            ( ?, ?, ?, DATETIME(), ?, 1);
+            ( ?, ?, ?, DATETIME(), ?, ?, 1);
         """, (
             new_post['user_id'], 
             new_post['category_id'], 
             new_post['title'],
+            new_post['image_url'],
             new_post['content'], )
         )
 
